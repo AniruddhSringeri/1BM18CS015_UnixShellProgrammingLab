66ddcdba99f042e180f8d3bd346c9b8da33c9d63
Aniruddh <aniruddhsringeri@gmail.com>
2020-11-05 14:59:40 +0530

    lab6/05-11-2020

---
+++ lab6/arithmeticOperators.sh
@@ -0,0 +1,19 @@
+echo "Enter Arithmetic Expression (a op b): "
+read a op b
+case $op in
+    "+") res=`expr $a + $b`
+    ;;
+    "-") res=`expr $a - $b`
+    ;;
+    "*") res=`expr $a \* $b`
+    ;;
+    "/") res=`expr $a / $b`
+    ;;
+    "%") res=`expr $a % $b`
+    ;;
+    *) echo "Invalid operator."
+    exit 1
+    ;;
+esac
+
+echo "$a $op $b = $res"
---
+++ lab6/vowel.sh
@@ -0,0 +1,16 @@
+echo "Enter an Alphabet: \c"
+read ch
+yes="$ch is a Vowel."
+case $ch in
+	a) echo "$yes";;
+	A) echo "$yes";;
+	e) echo "$yes";;
+	E) echo "$yes";;
+	i) echo "$yes";;
+	I) echo "$yes";;
+	o) echo "$yes";;
+	O) echo "$yes";;
+	u) echo "$yes";;
+	U) echo "$yes";;
+	*) echo "$ch is a Consonant.";;
+esac
