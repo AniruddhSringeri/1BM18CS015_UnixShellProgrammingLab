7785824a3d1742a475a6705dc167adf2a5829be7
Aniruddh <aniruddhsringeri@gmail.com>
2020-11-26 15:05:02 +0530

    concatenate

---
+++ lab7/concatenate.sh
@@ -0,0 +1,7 @@
+echo "Enter file sources: "
+read s1 s2
+echo "Enter file destination: "
+read dest
+cat $s1 | head -5 >>$dest
+cat $s2 | head -15 | tail -8 >>$dest
+echo "Successfully copied lines 1-5 of $s1 and 8-15 of $s2 into $dest."
